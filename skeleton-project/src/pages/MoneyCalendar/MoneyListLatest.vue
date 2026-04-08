50e0496ae903ada36119307a3cf071614f1bf49a
Hyeji Han <hj4094@naver.com>
2026-04-08 16:12:41 +0900

    Feat. 기본 화면/거래내역리스트 관련 라우트 초기 설정

--- skeleton-project/src/pages/MoneyCalendar/MoneyListLatest.vue
+++ skeleton-project/src/pages/MoneyCalendar/MoneyListLatest.vue
@@ -1,4 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>거래명</th>
+          <th>카테고리</th>
+          <th>금액</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
 </template>
 <script setup></script>
