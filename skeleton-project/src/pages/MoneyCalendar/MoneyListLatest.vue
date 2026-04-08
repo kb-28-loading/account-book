eb20fd50ccedeb17a7fd2315525447c91ac9d9fc
Hyeji Han <hj4094@naver.com>
2026-04-08 16:42:48 +0900

    Feat. 거래내역 최신순으로 보기 추가 Feat. 각 거래내역 상세화면 접근을 위한 listId를 db.json에 추가

--- skeleton-project/src/pages/MoneyCalendar/MoneyListLatest.vue
+++ skeleton-project/src/pages/MoneyCalendar/MoneyListLatest.vue
@@ -8,7 +8,22 @@
           <th>금액</th>
         </tr>
       </thead>
+      <tbody>
+        <router-link
+          :to="{ name: 'moneyItem' }"
+          v-for="item in loginStore.user.moneyList"
+          ><tr>
+            <td>{{ item.title }}</td>
+            <td>{{ item.category }}</td>
+            <td>{{ item.date }}</td>
+          </tr></router-link
+        >
+      </tbody>
     </table>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { useLoginStore } from '@/stores/login';
+
+const loginStore = useLoginStore();
+</script>
