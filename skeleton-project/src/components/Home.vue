08e209c7d83232dd34d69767ce358cc00872ee11
Hyeji Han <hj4094@naver.com>
2026-04-08 12:55:09 +0900

    Feat. 프로젝트 스켈레톤 업데이트

--- skeleton-project/src/components/Home.vue
+++ skeleton-project/src/components/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <h1>가계부 기본 화면</h1>
+    <Header />
+    <Budget />
+    <div>
+      <Calender />
+      <MoneyList />
+    </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import Calender from '@/pages/MoneyCalendar/Calender.vue';
+import Budget from './Budget.vue';
+import Header from './Header.vue';
+import MoneyList from './MoneyList.vue';
+</script>
