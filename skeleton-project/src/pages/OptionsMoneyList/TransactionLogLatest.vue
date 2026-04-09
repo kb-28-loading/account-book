8d7681f81d3a9a27c83b4154a7102f9a15fb50d3
강예찬 <qkdvo1530@gmail.com>
2026-04-09 11:00:31 +0900

    Feat. 거래 내역 조회 v-calendar

--- skeleton-project/src/pages/OptionsMoneyList/TransactionLogLatest.vue
+++ skeleton-project/src/pages/OptionsMoneyList/TransactionLogLatest.vue
@@ -3,8 +3,19 @@
     <h1>거래 내역 조회</h1>
     <hr />
     <h3>최신순</h3>
-    <h3>최신순</h3>
-    <h3>최신순</h3>
-    <h3>최신순</h3>
+    <h3>주간</h3>
+    <h3>월간</h3>
+    <div>
+      <!-- 날짜 표시 + 버튼 -->
+      <span>-</span>
+      <button @click="showCalendar = !showCalendar">📅</button>
+
+      <!-- 달력 팝업 -->
+      <div v-if="showCalendar">
+        <input type="date" v-model="startDate" />
+        <input type="date" v-model="endDate" />
+      </div>
+    </div>
   </div>
 </template>
+<script setup></script>
