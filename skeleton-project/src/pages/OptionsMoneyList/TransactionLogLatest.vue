419f63579283471e6d9f520450d332a0aad46656
강예찬 <qkdvo1530@gmail.com>
2026-04-09 13:25:36 +0900

    Feat. 거래 내역 조회 버튼 부분 세부화

--- skeleton-project/src/pages/OptionsMoneyList/TransactionLogLatest.vue
+++ skeleton-project/src/pages/OptionsMoneyList/TransactionLogLatest.vue
@@ -1,21 +1,36 @@
 <template>
   <div>
-    <h1>거래 내역 조회</h1>
-    <hr />
-    <h3>최신순</h3>
-    <h3>주간</h3>
-    <h3>월간</h3>
     <div>
-      <!-- 날짜 표시 + 버튼 -->
-      <span>-</span>
-      <button @click="showCalendar = !showCalendar">📅</button>
+      <h1>거래 내역 조회</h1>
+      <hr />
+    </div>
+    <div>
+      <div @click=""><h5>최신순</h5></div>
+      <div @click=""><h5>주간</h5></div>
+      <div @click=""><h5>월간</h5></div>
 
-      <!-- 달력 팝업 -->
-      <div v-if="showCalendar">
-        <input type="date" v-model="startDate" />
-        <input type="date" v-model="endDate" />
+      <!-- 날짜 표시 + 버튼 -->
+      <div class="date-box">
+        {{ startDate }} - {{ endDate }}
+        <button @click="showCalendar = !showCalendar">📅</button>
+        <button @click="inputData()">조회</button>
+      </div>
+
+      <!-- font awesome 적용 전 임시 -->
+      <div>
+        <!-- 달력 팝업 -->
+        <div v-if="showCalendar">
+          <input type="date" v-model="startDate" />
+          <input type="date" v-model="endDate" />
+        </div>
       </div>
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const showCalendar = ref(false);
+const startDate = ref("start");
+const endDate = ref("end");
+const inputData = ref(() => {});
+</script>
