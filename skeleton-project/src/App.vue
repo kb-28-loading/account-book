b5a51c19d7c64edd241fa89be11543973e475f67
신송윤 <som4668@gmail.com>
2026-04-09 17:43:58 +0900

    Feat. 예산 틀 수정 (신송윤)

--- skeleton-project/src/App.vue
+++ skeleton-project/src/App.vue
@@ -1,24 +1,27 @@
 <template>
-  <RouterLink :to="{ name: 'login' }">로그인</RouterLink>
+  <Practice />
+  <!-- <RouterLink :to="{ name: 'login' }">로그인</RouterLink>
   <RouterLink :to="{ name: 'home' }">홈</RouterLink>
   <RouterLink :to="{ name: 'options' }">옵션</RouterLink>
-  <RouterView @login-success="goHome" @go-join="goJoin"></RouterView>
+  <RouterView @login-success="goHome" @go-join="goJoin"></RouterView> -->
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import Practice from "./pages/BudgetSetting/Practice.vue";
+import practice from "./pages/BudgetSetting/Practice.vue";
+import { useRouter } from "vue-router";
 const router = useRouter();
 
 // 로그인 성공 시 보이는 화면을 '/home'으로 변경
 const goHome = () => {
   // console.log('함수 실행');
   // console.log(router);
-  router.replace('/home');
+  router.replace("/home");
 };
 
 // 회원가입 페이지로 이동
 const goJoin = () => {
-  router.push('/join');
+  router.push("/join");
 };
 </script>
 
--- skeleton-project/src/pages/BudgetSetting/Budget.vue
+++ skeleton-project/src/pages/BudgetSetting/Budget.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <h2>예산 확인 화면</h2>
+  <div class="budget-box">
+    <!-- 1번 : 전체 박스 -->
+    <div class="title">예산관리 <span class="sub">예산확인</span></div>
+    <!-- 2번 : 제목 -->
+
+    <div class="month-box">
+      <!-- 3번 : 월 이동  -->
+      <div class="budget-box"></div>
+      <span class="arrow" @click="prevMonth">←</span>
+      <span class="month-text">{{ month }}월</span>
+      <span class="arrow" @click="nextMonth">→</span>
+      <div class="budget-title">전체예산 :</div>
+    </div>
   </div>
 </template>
 
