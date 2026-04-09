a0b7a5f65e922256f5d1e78214383c9a627ffd6b
jinseo <acgrp103@gmail.com>
2026-04-09 10:05:40 +0900

    Feat. 장진서 날짜클릭시 변경 안되는 오류 수정

--- skeleton-project/src/components/Calender.vue
+++ skeleton-project/src/components/Calender.vue
@@ -1,4 +1,6 @@
 <script setup>
+// =====================================================================
+// 1. 오늘 날짜의 데이터를 담기
 const today = new Date();
 
 console.log(today);
@@ -14,29 +16,36 @@ console.log(month); /* 3 */
 // 오늘에 대한 데이터에서 월에 대한 데이터 반환
 // -> 하지만 JS에서는 getMonth를 사용할경우(0~11)으로 반환 (0=1월)~(11=12월)
 
+// ===============================================================================
+// 1-1. 이번달 시작점과 끝지점 구하기
+
 const firstDay = new Date(year, month, 1).getDay();
-// 1. 이번 달의 첫날 요일 (0: 일요일, 1: 월요일...)
+// 이번 달의 첫날 요일 (0: 일요일, 1: 월요일...)
 // new Date(2026, 3, 1).getDay()   4월 1일은 무슨 요일인가?   3(수요일)
 // **getDay()**는 요일(일~토)을 숫자로 가져오고,
 
 const lastDate = new Date(year, month + 1, 0).getDate();
-// 2. 이번 달의 마지막 날짜
+// 이번 달의 마지막 날짜
 // new Date(2026, 4, 0).getDate()   4월의 마지막 날짜는 며칠인가?   30(30일)
 // **getDate()**는 날짜(1~31)를 숫자로 가져옵니다.
 
+// ================================================================================
+// 2. 달력 만들기
 const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// 요일 배열
 
 let dates = [];
-// 전체 달력 (주 단위 배열들을 모아놓은 2차원 배열)
+// 전체 달력 (주 단위 배열들을 모아놓은 2차원 배열) ㅁ
 
 let week = [];
-// 한 주 (7개의 칸을 담는 1차원 배열)
+// 한 주 (7개의 칸을 담는 1차원 배열) ㅡ
 
+// 2-1. 1일 이전 채우기
 for (let i = 0; i < firstDay; i++) {
   week.push('');
 }
-// 1일이 해당하는 위치 전에 빈칸 넣기
 
+// 2-2. 달력 채우기
 for (let i = 1; i <= lastDate; i++) {
   // 1일부터 당월을 다 채울 때까지 진행
 
@@ -53,6 +62,7 @@ for (let i = 1; i <= lastDate; i++) {
   }
 }
 
+// 2-3. 막주 채우기
 if (week.length > 0) {
   // 당월을 다 채우고 나왔는데 마지막 주가 7칸을 다 채우지 못하고 '남아' 있을 경우
 
@@ -64,13 +74,16 @@ if (week.length > 0) {
 }
 
 // ===========================================================================
+// 3. Router 연결하기(index.js)
+// Router_Link로 연결될 to들을 어디로 연결할지 정하는 함수
 
-// const router = useRouter();
-
+// 2026-4-1을 2026-04-01로 으로 사용하기 위한 함수
 const getFormattedDate = (day) => {
-  // 2026-4-1을 2026-04-01로 으로 사용하기 위해 사용
-
   if (!day) return '';
+  // 빈칸 누르거나 잘못 누를경우 아무것도 안함
+
+  const fyear = String(year);
+  // String(value) : 숫자인 월을 문자로 변환
 
   const fMonth = String(month + 1).padStart(2, '0');
   // String(value) : 숫자인 월을 문자로 변환
@@ -79,12 +92,12 @@ const getFormattedDate = (day) => {
   const fDay = String(day).padStart(2, '0');
   // String(value) : 숫자인 일을 문자로 변환
   // padStart(2, "0") : 문자열의 길이를 2자로 만듬, 빈칸은 "0"으로 채움
-  console.log(`${year}-${fMonth}-${fDay}`);
+  console.log(`${fyear}-${fMonth}-${fDay}`);
 
-  return `${year}-${fMonth}-${fDay}`;
+  return `${fyear}-${fMonth}-${fDay}`;
 };
 </script>
-
+<!-- 4. 화면 만들기 -->
 <template>
   <div>
     <h1>{{ year }}년 {{ month + 1 }}월</h1>
@@ -96,6 +109,7 @@ const getFormattedDate = (day) => {
 
       <tr>
         <th v-for="value in daysOfWeek" :key="value">{{ value }}</th>
+        <!-- 요일 배열 가져다가 넣기 -->
       </tr>
 
       <tr v-for="(week, index) in dates" :key="index">
@@ -104,13 +118,17 @@ const getFormattedDate = (day) => {
         <td v-for="(day, dIdx) in week" :key="dIdx">
           <!-- 
           (day, dIdx) in week : week 배열에서 값(day)뿐만 아니라 해당 요소의 순서 번호인
-          인덱스(dIdx)를 꺼냅니다. 
+          인덱스(dIdx)를 꺼냄. 
           -->
           <router-link
             v-if="day"
             :to="{
+              // 만들어 질때 to에 해당하는 곳으로 링크 만들어둠 -> 누르면 이동
               name: 'moneyListDaily',
+              // index.js에 있는 moneyListDaily사용
               params: { selectedDate: getFormattedDate(day) },
+              // 동기 방식 : 날짜 클릭시 getFormattedDate()함수에서
+              //            눌린 날 정보를 return함
             }"
           >
             {{ day }}
@@ -119,6 +137,7 @@ const getFormattedDate = (day) => {
           </router-link>
 
           <span v-else></span>
+          <!-- 만약 day가 ''이라면 -->
         </td>
       </tr>
     </table>
@@ -127,11 +146,14 @@ const getFormattedDate = (day) => {
 <style scoped>
 table {
   width: 60%;
-  border-collapse: collapse; /* 테두리 사이 간격 제거 */
+  border-collapse: collapse;
+  /* 테두리 사이 간격 제거 */
 }
+
 th,
 td {
-  border: 1px solid black; /* 명확한 검은색 줄 추가 */
+  border: 1px solid black;
+  /* 명확한 검은색 줄 추가 */
   padding: 10px;
   text-align: center;
   vertical-align: top;
