8d2075d6733f7df0e02d1fa733485edab958e9b7
Hyeji Han <hj4094@naver.com>
2026-04-10 10:03:11 +0900

    Feat. Header Css 완료. RightNavBar 기능 구현 전

--- skeleton-project/src/components/Header.vue
+++ skeleton-project/src/components/Header.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    Header
-    <hr />
+    <img src="@/assets/mainLogo.jpg" alt="메인 로고" />
+    <RightNavBar />
   </div>
 </template>
-<script setup></script>
+<script setup>
+import RightNavBar from './RightNavBar.vue';
+</script>
+<style scoped>
+div {
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+}
+img {
+  width: 113px;
+  border-radius: 60px;
+}
+</style>
